9fc80473148c9fb81e4f833fc6f674875e3013c4
srinivasan-M <srinivasanksr15@gmail.com>
2017-05-01 16:17:37 +0530

    Create ascii.java

---
+++ ascii.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class ascii
+{
+public static void main(String args[]){
+String ch;
+for(int i=1;i<=255;i++){
+ch=new Character((char)i).toString();
+System.out.println(i+":"+ch)
+}}}
